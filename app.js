a3cc651bdd42c9865e4d1472f8a6ca95ae4db7b3
Allaniyazov Sultanbek <allaniyazov.sultan@mail.ru>
2023-11-21 17:26:10 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+
+var swiper = new Swiper(".swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    cssMode: true,
+    loop: true,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+    },
+});
